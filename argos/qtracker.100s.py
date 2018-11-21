715b7c1896beb87f83547f8712f5ee37e2774eab
shihadt <shihad@qburst.com>
2018-11-21 10:28:58 +0530

    added in time and corrected break duration

--- argos/qtracker.100s.py
+++ argos/qtracker.100s.py
@@ -4,14 +4,28 @@ import httplib2
 import datetime
 import time
 import json
+import os
 
 
 AUTH_KEY = ''
-REFRESH_RATE = 100
+REFRESH_RATE = 1
 def format(num):
-    if num < 9:
+    if num <= 9:
         return '0'+ str(num)
     return str(num)
+
+def toIST(t):
+	hour = int(t[:2]) + 5
+	minute = int(t[3:])+30
+	if minute >=60:
+		hour=hour+1
+		minute = minute-60
+	return format(hour) + ":" + format(minute)
+
+def minuteToHour(m):
+	hour = int(m/60)
+	minute = int(m%60)
+	return format(hour)+":"+format(minute)
 
 def changeToTime(num):
     hour = int(num)
@@ -25,10 +39,15 @@ def setTitle(data):
 
 def currectStatus(data):
     daily_logs = len(data['daily_logs']) -1
+
+    yourdate = toIST(data['first_in_time'][11:16])
+
+
     if int(data['daily_logs'][daily_logs]['in_out']) == 0:
-        print("IN")
+        print("IN" + " :: " + yourdate)
     else:
-        print("OUT")
+        print("OUT" + " :: " + yourdate)
+
 
 def printData(data):
     day = len(data['payload']['monthlyData'])-1
@@ -38,8 +57,10 @@ def printData(data):
     currectStatus(today)
     print("Burned Time   : " + changeToTime(today['hours_burned']))
     print("Clocked Time  : " + changeToTime(today['hours_clocked']))
-    print("Break Duration: " + changeToTime(today['break_duration']))
+    print("Break Duration: " + minuteToHour(today['break_duration']))
 
+    # if int(today['hours_clocked'])>=9:
+    #     sendNotification()
 
 def apiCall():
     
@@ -52,6 +73,8 @@ def apiCall():
     
     data = json.loads(content)
     printData(data)
+
+    
 
 def main():
     apiCall()
